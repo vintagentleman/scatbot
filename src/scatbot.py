e1a205f8fc799703c24afe7783e58f5fd3a5280f
Konstantin Sipunin <k.sipunin@just-ai.com>
2019-05-02 18:29:09 +0300

    OOP refactor

--- src/scatbot.py
+++ src/scatbot.py
@@ -1,36 +1,53 @@
 import os
 import sys
 
+import psycopg2
+import yaml
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-from handlers import *
-from utils import logger
+from utils import logger, get_db_url
 
-TOKEN = os.getenv('TOKEN')
-mode = os.getenv('MODE')
 
-if mode == 'dev':
-    def run(app):
-        app.start_polling()
-elif mode == 'prod':
-    def run(app):
-        PORT = int(os.environ.get('PORT', '8443'))
-        HEROKU_APP_NAME = os.environ.get('HEROKU_APP_NAME')
-        app.start_webhook(listen='0.0.0.0', port=PORT, url_path=TOKEN)
-        app.bot.set_webhook('https://{}.herokuapp.com/{}'.format(HEROKU_APP_NAME, TOKEN))
-        app.idle()
-else:
-    logger.error('Invalid MODE value')
-    sys.exit(1)
+class Bot:
+
+    def _start_callback(self, update, context):
+        logger.info('User {} started bot'.format(update.effective_user['id']))
+        context.bot.send_message(chat_id=update.message.chat_id, text=self._answers['start'])
+
+    def _echo_callback(self, update, context):
+        logger.info('User {} echoed {}'.format(update.effective_user['id'], update.message.text))
+        context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+    def __init__(self):
+        self._token = os.getenv('TOKEN')
+        self._db_conn = psycopg2.connect(os.environ['DATABASE_URL'] if MODE == 'prod' else get_db_url('scatbot'))
+        self._updater = Updater(self._token, use_context=True, request_kwargs={'proxy_url': os.getenv('PROXY')})
+
+        self._updater.dispatcher.add_handler(CommandHandler('start', self._start_callback))
+        self._updater.dispatcher.add_handler(MessageHandler(Filters.text, self._echo_callback))
+
+        with open('answers.yaml', encoding='utf-8') as answers_conf:
+            self._answers = yaml.load(answers_conf, Loader=yaml.Loader)
+
+    def run(self):
+        logger.info('Starting bot')
+
+        if MODE == 'dev':
+            self._updater.start_polling()
+        else:
+            port = int(os.environ.get('PORT', '8443'))
+            heroku_app_name = os.environ.get('HEROKU_APP_NAME', 'scatbot')
+            self._updater.start_webhook(listen='0.0.0.0', port=port, url_path=self._token)
+            self._updater.bot.set_webhook('https://{}.herokuapp.com/{}'.format(heroku_app_name, self._token))
+
+        self._updater.idle()
 
 
 if __name__ == '__main__':
-    logger.info('Starting bot')
-    updater = Updater(TOKEN, use_context=True, request_kwargs={
-        'proxy_url': os.getenv('PROXY')
-    })
+    MODE = os.getenv('MODE')
 
-    updater.dispatcher.add_handler(CommandHandler('start', start_handler))
-    updater.dispatcher.add_handler(MessageHandler(Filters.text, echo_handler))
+    if MODE not in ('dev', 'prod'):
+        logger.error('Invalid MODE value. Aborting')
+        sys.exit(1)
 
-    run(updater)
+    Bot().run()
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,26 @@
+import sys
 import logging
+import subprocess
 from logging import config
 from pathlib import Path
 
 from yaml import load, Loader
 
 
+def get_db_url(name):
+    proc = subprocess.run(f'heroku config:get DATABASE_URL -a {name}', capture_output=True, shell=True, text=True)
+
+    if proc.returncode != 0 or proc.stdout is None:
+        logger.error('Failed to retrieve Heroku database URL. Aborting')
+        sys.exit(1)
+    else:
+        logger.info('Successfully retrieved Heroku database URL')
+        return proc.stdout.strip()
+
+
 def config_logger(name):
-    conf_path = Path.joinpath(Path(__file__).resolve().parents[1], 'conf', 'logging.yaml')
-
-    with conf_path.open() as conf_file:
-        config.dictConfig(load(conf_file, Loader=Loader))
-
+    with Path.joinpath(Path(__file__).resolve().parents[1], 'conf', 'logging.yaml').open() as logging_conf:
+        config.dictConfig(load(logging_conf, Loader=Loader))
     return logging.getLogger(name)
 
 
